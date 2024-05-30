2d24029bbc5a7dc93f11d85224b00b8a9c99b8ca
sbchild <sbchild0@gmail.com>
2024-05-30 20:36:01 +0800

    death for catmatrix

--- src/home/Death.tsx
+++ src/home/Death.tsx
@@ -1,11 +1,11 @@
 import { ReactComponent as OauLogo } from '@/assets/icons/oau.svg';
 import { ReactComponent as TelegramLogo } from '@/assets/icons/telegram.svg';
+import ImgCatMatrix from '@/assets/images/death/cat_matrix.jpg';
 import ImgStarmoe from '@/assets/images/death/starmoe.jpg';
 import Anim from '@/components/animated';
 import Icon, { GithubOutlined } from '@ant-design/icons';
-import { Avatar, Card, Col, Divider, Row, Typography } from 'antd';
+import { Avatar, Card, Col, Divider, Row, Typography, Watermark } from 'antd';
 import React from 'react';
-import { useNavigate } from 'umi';
 
 function LinkTitle(props: {
   icon: React.ReactNode;
@@ -13,7 +13,7 @@ function LinkTitle(props: {
   target?: string;
 }) {
   return props.target ? (
-    <a href={props.target} target="_blank">
+    <a href={props.target} target="_blank" rel="noreferrer">
       <Typography.Paragraph underline>
         {props.icon} {props.title}
       </Typography.Paragraph>
@@ -24,54 +24,62 @@ function LinkTitle(props: {
 function DeathCard(props: {
   title: string;
   desc: string;
-  deathDate?: string;
+  deathDate?: string | false;
   avatar: string;
   githubLink?: string;
   tgLink?: string;
-  oauLink?: string; // one-among.us
+  oauLink?: string | false; // one-among.us
   extra?: React.ReactNode;
 }) {
   return (
     <Col xs={24} sm={24} md={12}>
-      <Card hoverable>
-        <Card.Meta
-          avatar={<Avatar src={props.avatar} size={50} />}
-          title={props.title}
-          description={
-            <div>
+      <Watermark
+        content={props.deathDate === false ? ['尚未升天', '敬请期待'] : []}
+      >
+        <Card hoverable>
+          <Card.Meta
+            avatar={<Avatar src={props.avatar} size={50} />}
+            title={props.title}
+            description={
               <div>
-                升天日期: {props.deathDate ? props.deathDate : '已失传'}
+                {props.deathDate === false ? (
+                  <div>尚未升天, 敬请期待</div>
+                ) : (
+                  <div>
+                    升天日期: {props.deathDate ? props.deathDate : '已失传'}
+                  </div>
+                )}
               </div>
-            </div>
-          }
-        />
-        <Divider orientation="left">生平经历</Divider>
-        <div>{props.desc}</div>
-        <Divider orientation="left">天堂电话</Divider>
-        <LinkTitle
-          icon={<Icon component={TelegramLogo} />}
-          title="Telegram"
-          target={props.tgLink}
-        />
-        <LinkTitle
-          icon={<GithubOutlined />}
-          title="GitHub"
-          target={props.githubLink}
-        />
-        <LinkTitle
-          icon={<Icon component={OauLogo} viewBox="0 0 593.33 604" />}
-          title="One Among Us"
-          target={props.oauLink}
-        />
-        {props.extra ? <div>{props.extra}</div> : <div />}
-      </Card>
+            }
+          />
+          <Divider orientation="left">生平经历</Divider>
+          <div>{props.desc}</div>
+          <Divider orientation="left">天堂电话</Divider>
+          <LinkTitle
+            icon={<Icon component={TelegramLogo} />}
+            title="Telegram"
+            target={props.tgLink}
+          />
+          <LinkTitle
+            icon={<GithubOutlined />}
+            title="GitHub"
+            target={props.githubLink}
+          />
+          <LinkTitle
+            icon={<Icon component={OauLogo} viewBox="0 0 593.33 604" />}
+            title={
+              'One Among Us' + (props.oauLink === false ? ' (敬请期待)' : '')
+            }
+            target={props.oauLink === false ? '.' : props.oauLink}
+          />
+          {props.extra ? <div>{props.extra}</div> : <div />}
+        </Card>
+      </Watermark>
     </Col>
   );
 }
 
-export default function PlayContent(props: {}) {
-  const nav = useNavigate();
-
+export default function DeathContent(props: {}) {
   return (
     <Anim k="root-death" className="__content__">
       <Typography.Title>色天堂</Typography.Title>
@@ -88,6 +96,15 @@ export default function PlayContent(props: {}) {
           avatar={ImgStarmoe}
           githubLink="https://github.com/Bro-Xun"
           tgLink="https://t.me/meow_starmoe"
+        />
+        <DeathCard
+          title="猫猫(CatMatrix)"
+          desc={'"一个失败的 MtF"? 和艾伦·图灵相似的死法。'}
+          deathDate={false}
+          avatar={ImgCatMatrix}
+          githubLink="https://github.com/Butyllithium"
+          tgLink="https://t.me/CatMatrix_QAQ"
+          oauLink={false}
         />
       </Row>
       <Divider />
